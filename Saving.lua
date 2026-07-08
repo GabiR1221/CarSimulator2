c5b9ea917c26a6a0f5cb3f002bff87dd6ce6f795
GabiR1221 <gabrielrusu0411@gmail.com>
2026-07-09 00:46:03 +0300

    Update Saving.lua

--- Saving.lua
+++ Saving.lua
@@ -6,23 +6,26 @@ local SavingFunctions = {}
 
 SavingFunctions.SaveData = function(Player, AutoSave)
 	local Folder = Player.Data
-		
+
 	if Player:FindFirstChild("Loaded") == nil or Player.Loaded.Value == false then
 		return
 	end
-	
-	Player.Loaded.Value = false
-	
+
+	local wasLoaded = Player.Loaded.Value
+	if AutoSave then
+		Player.Loaded.Value = false
+	end
+
 	local Save = {}
-		
+
 	for FolderName,FolderInfo in require(script.Parent.Values).SaveValues do
 		Save[FolderName] = {}
-		
+
 		for _,Info in FolderInfo do
 			Save[FolderName][Info.ID] = Folder[FolderName][Info.Name].Value
 		end
 	end
-	
+
 	Save["Pets"] = {}
 	for _,Pet in Folder.Pets:GetChildren() do
 		Save["Pets"][tonumber(Pet.Name)] = {
@@ -30,7 +33,7 @@ SavingFunctions.SaveData = function(Player, AutoSave)
 			Equipped = Pet.Equipped.Value,
 		}
 	end
-	
+
 	Save["AutoDelete"] = {}
 	for _,Pet in Folder.AutoDelete:GetChildren() do
 		if Pet.Value then
@@ -39,22 +42,53 @@ SavingFunctions.SaveData = function(Player, AutoSave)
 	end
 
 	Save["Vehicles"] = {}
-	for _, vehicle in Folder.Vehicles:GetChildren() do
-		if vehicle.Value then
-			Save["Vehicles"][vehicle.Name] = true
+	if Folder:FindFirstChild("Vehicles") then
+		for _, vehicle in Folder.Vehicles:GetChildren() do
+			if vehicle:IsA("BoolValue") then
+				Save["Vehicles"][vehicle.Name] = vehicle.Value == true
+			end
 		end
 	end
-	
+
+	Save["VehicleCustomizations"] = {}
+	if Folder:FindFirstChild("VehicleCustomizations") then
+		for _, vehicleFolder in Folder.VehicleCustomizations:GetChildren() do
+			if vehicleFolder:IsA("Folder") then
+				Save["VehicleCustomizations"][vehicleFolder.Name] = {}
+				for _, equipped in vehicleFolder:GetChildren() do
+					if equipped:IsA("StringValue") and equipped.Value ~= "" then
+						Save["VehicleCustomizations"][vehicleFolder.Name][equipped.Name] = equipped.Value
+					end
+				end
+			end
+		end
+	end
+
+	Save["VehicleCustomizations"] = {}
+	for _, vehicleFolder in Folder.VehicleCustomizations:GetChildren() do
+		Save["VehicleCustomizations"][vehicleFolder.Name] = {}
+		for _, value in vehicleFolder:GetChildren() do
+			if value:IsA("StringValue") and value.Value ~= "" then
+				Save["VehicleCustomizations"][vehicleFolder.Name][value.Name] = value.Value
+			end
+		end
+	end
+
 	if AutoSave then
 		Save.SessionId = game.JobId
 		Save.LastInGame = os.time()
 	end
-	
+
 	local suc,er = pcall(function()
 		DS:GetDataStore(RS["Game Settings"].DataSave.Value):SetAsync(Player.UserId, Save)
 	end)
-	
-	if er then warn("error with saving data for "..Player.Name.." : "..er) end
+
+	if er then
+		warn("error with saving data for "..Player.Name.." : "..er)
+		if AutoSave and Player.Parent then
+			Player.Loaded.Value = wasLoaded
+		end
+	end
 end
 
 return SavingFunctions
